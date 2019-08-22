233fba3b11a87b009d19e47a66e53ed9f0353142
betty <s9654025@gmail.com>
2019-08-22 20:30:45 +0800

    Updates

---
+++ precache-manifest.af0abeccabb0d3c6f54be8cec4bf7901.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb950c639013a7918485ae870be3c7c",
+    "url": "/payment-app/index.html"
+  },
+  {
+    "revision": "4377757d15f0e73cb173",
+    "url": "/payment-app/static/css/main.436d0371.chunk.css"
+  },
+  {
+    "revision": "9a12d8d5c7d12d190eaa",
+    "url": "/payment-app/static/js/2.1230d522.chunk.js"
+  },
+  {
+    "revision": "4377757d15f0e73cb173",
+    "url": "/payment-app/static/js/main.5d32391a.chunk.js"
+  },
+  {
+    "revision": "66ce4fbe8d94287d59cd",
+    "url": "/payment-app/static/js/runtime~main.4a6b527f.js"
+  }
+]);
